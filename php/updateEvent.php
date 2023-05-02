8aa66883c69eb54b510ace7d1ef1e3c86d0a408d
Davide <bartali.coppi@gmail.com>
2023-05-02 22:39:43 +0200

    aggiunti puntini

---
+++ php/updateEvent.php
@@ -0,0 +1,11 @@
+<?php
+$dbconn = pg_connect("host=localhost password=biar user=postgres port=5432 dbname=StartConnect") 
+        or die("Errore di connessione:  . pg_last_error()");
+$result = pg_query($dbconn, "SELECT nome,descrizione,latitudine, longitudine FROM evento");
+$coordinate = array();
+while ($row = pg_fetch_assoc($result)) {
+    $coordinate[] = $row;
+}
+header('Content-Type: application/json');
+echo json_encode($coordinate);
+?>
