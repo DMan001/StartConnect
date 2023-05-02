93a8c375cc9ba3c53e054559d9807074fbbece5c
Davide <bartali.coppi@gmail.com>
2023-05-02 22:58:23 +0200

    eliminato for in utile

--- javascript/map.js
+++ javascript/map.js
@@ -27,6 +27,8 @@ function updateMap(locations) {
   for (var i = 0; i < locations.length; i++) {
     var location = locations[i];
     var marker = {
+      name: location.nome,
+      descrizione: location.descrizione,
       latLng: [location.latitudine, location.longitudine],
       style: {
         fill: 'yellow',
@@ -35,33 +37,21 @@ function updateMap(locations) {
         r: 6
       },
     };
-    for (var i = 0; i < locations.length; i++) {
-      var location = locations[i];
-      var marker = {
-        name: location.nome,
-        descrizione: location.descrizione,
-        latLng: [location.latitudine, location.longitudine],
-        style: {
-          fill: 'yellow',
-          stroke: 'black',
-          'stroke-width': 1,
-          r: 6
-        }
-      };
-      map.addMarker(i, marker);
-      var markerElement = $('#map').find('.jvectormap-marker[data-index="' + i + '"]');
-      $(markerElement).data('style', marker.style);
-      $(markerElement).data('name', marker.name);
-      $(markerElement).data('descrizione', marker.descrizione);
-      $(markerElement).click(function() {
-        var descrizione= $(this).data('descrizione');
-        var name = $(this).data('name');
-        $('#text-content').html(descrizione);
-        $('#nome').html(name);
-      });
-    }
+
+  map.addMarker(i, marker);
+  var markerElement = $('#map').find('.jvectormap-marker[data-index="' + i + '"]');
+  $(markerElement).data('style', marker.style);
+  $(markerElement).data('name', marker.name);
+  $(markerElement).data('descrizione', marker.descrizione);
+  $(markerElement).click(function() {
+    var descrizione= $(this).data('descrizione');
+    var name = $(this).data('name');
+    $('#text-content').html(descrizione);
+    $('#nome').html(name);
+  });
   }
 }
+
 
 
 
