cb9c630f72bcfe9ad7c2a9cd7793af86d666b8af
Davide <bartali.coppi@gmail.com>
2023-05-16 13:57:33 +0200

    messo codice html in php

--- area.php
+++ area.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+//se l'utente è loggato mostra la pagina personale,in caso contrario mostra una pagina in cui lo inviti a loggarsi
+if(isset($_SESSION['loggedin'])==false) {
+    header("Location: ../log.html");
+}
+?>
 <!DOCTYPE html>
 <html >
   <head>
